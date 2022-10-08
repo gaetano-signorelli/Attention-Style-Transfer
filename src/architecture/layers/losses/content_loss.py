Fixed normalization

src/architecture/layers/losses/content_loss.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
+from src.architecture.layers.norm_layer import NormalizeLayer
 from src.architecture.layers.losses.mse_loss import MSELossLayer
 
 class ContentLossLayer(layers.Layer):
@@ -9,7 +10,7 @@ class ContentLossLayer(layers.Layer):
 
         super(ContentLossLayer, self).__init__()
 
-        self.norm_layer = layers.Normalization(axis=(1,2))
+        self.norm_layer = NormalizeLayer(axis=(1,2))
         self.mse_layer = MSELossLayer()
 
     @tf.function
@@ -20,8 +21,8 @@ class ContentLossLayer(layers.Layer):
         x = inputs[0]
         target = inputs[1]
 
-        #x = self.norm_layer(x)
-        #target = self.norm_layer(target)
+        x = self.norm_layer(x)
+        target = self.norm_layer(target)
 
         loss = self.mse_layer([x, target])
 